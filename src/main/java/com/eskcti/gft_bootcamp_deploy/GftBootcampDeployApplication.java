afe07557f12cb2ad5e71e2bc48f4de626da5685f
eskokado <eskokado@gmail.com>
2024-06-05 14:33:22 -0300

    initial commit

---
+++ src/main/java/com/eskcti/gft_bootcamp_deploy/GftBootcampDeployApplication.java
@@ -0,0 +1,13 @@
+package com.eskcti.gft_bootcamp_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GftBootcampDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GftBootcampDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eskcti/gft_bootcamp_deploy/GftBootcampDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskcti.gft_bootcamp_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GftBootcampDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
